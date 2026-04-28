f6ef08c01f318ea50f8e4e4562fab3f85e4bc35e
Jose Tondin <josevictortondin2006@gmail.com>
2026-04-28 11:37:18 -0300

    Apenas ajustando os commits para intercalar

--- src/JE10_LOOPS/For.java
+++ src/JE10_LOOPS/For.java
@@ -3,10 +3,27 @@ package JE10_LOOPS;
 public class For {
     static void main(String[] args) {
         //usando for, é bem legal, ao declarar a variavel (int i = 0;), esse é onde o for começa e ja faz a primeira execução, cada ; indica que o for esta fazendo uma operação, geralmente são 3 operações. A segunda parte do for é geralmente onde é definido onde ele vai PARAR, para nao ficar em LOOP INFINITO, nesse caso é o tamanho do ARRAY em INDICES, enquanto i for menor que o tamanho da lista alunos, vai executando os comandos. E o ultimo bloco é o incremento, pois enquanto i for menor, ele em algum momento tem que ser maior, aí que entra o incremento, todo fim de execucao, ele incrementa.
-        String alunos[] = { "Felipe", "Bambos", "Joseph", "Jonas" };
+        String[] alunos = { "Felipe", "Bambos", "Joseph", "Jonas" };
+
+        System.out.println(alunos.length);
+
+        char[] array = {'r','a','d', 'a', 'r'};
+        boolean palindromo = true;
+
+        for (int i = 0; i<array.length / 2; i++ ) {
+
+            int indiceFinal = array.length - 1 - i;
+
+            if (array[indiceFinal] != array[i]) {
+                palindromo = false;
+                break;
+            }
+        }
+        System.out.println(palindromo);
 
         for (int i=0; i < alunos.length; i++) {
-            System.out.println(i==0 ? "Aluno" + i + " = " + alunos[i] : "Alunos" + i + " = " + alunos[i]);
+            int numero = i + 1;
+            System.out.println(i==0 ? "Aluno" + numero + " = " + alunos[i] : "Alunos" + numero + " = " + alunos[i]);
         }
 
         //Aqui temos o famoso FOREACH, ele já é próprio para Arrays/Listas/Coleções basicamente, não é igual javascript que tem um for chamado forEach, mas aqui o forEach é dado com os : basicamente, criando uma Variavel String antes : array, não é muito recomendado para ver indices etc.
